e8484e15570326f2b7d88cfa24bd01d7163b31cc
S. Zavala <szavala3@outlook.com>
2021-04-13 10:32:37 -0400

    Add init method

--- shape_calculator.py
+++ shape_calculator.py
@@ -4,7 +4,9 @@ Shape Calculator by Sofia Zavala
 
 
 class Rectangle:
-    pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 class Square(Rectangle):
     pass
