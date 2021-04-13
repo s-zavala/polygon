46c162309e1554dd153fc166def3f51fd4242c35
S. Zavala <szavala3@outlook.com>
2021-04-13 12:33:14 -0400

    Add methods for height, width, area, diagonal, perim, picture, amt inside

--- shape_calculator.py
+++ shape_calculator.py
@@ -7,6 +7,42 @@ class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+    
+    def set_width(self, new_width):
+        pass
+    
+    def set_height(self, new_height):
+        pass
+
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (width ** 2 + height ** 2) ** 0.5
+    
+    def get_picture(self):
+        return ('\*' * self.width + '\n') * self.height
+
+    def get_amount_inside(self, other_shape):
+        pass
+
+    def __str__(self):
+        return f"Rectangle: width={self.width}, height={self.height}"
 
 class Square(Rectangle):
-    pass
+    def __init__(self, side):
+        Rectangle.__init__(self, width=side, height=side)
+    
+    def set_height(self, new_height):
+        Rectangle.set_height(new_height)
+        Rectangle.set_width(new_height)
+    
+    def set_width(self, new_width):
+        Rectangle.set_width(new_width)
+        Rectangle.set_height(new_width)
+
+    def __str__(self):
+        return f"Square: side={self.width}"
